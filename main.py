aee4f1446e5a73e70b7ad2df519dccbe62f11df5
neverdie <altiore@mail.ru>
2019-05-01 11:07:35 +0300

    Add TG bot messages.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
-from dotenv import load_dotenv
 import os
+import telegram
+from dotenv import load_dotenv
+
 load_dotenv()
 
 
 def main():
+    chat_id = 424079888
+    bot = telegram.Bot(token=os.getenv('BOT_TOKEN'))
+
     while True:
         url = 'https://dvmn.org/api/long_polling/'
         headers = {
-            'Authorization': f"Token {os.getenv('TOKEN')}"
+            'Authorization': f"Token {os.getenv('DEV_TOKEN')}"
         }
         params = {}
         try:
@@ -21,7 +26,14 @@ def main():
                 lesson_title = new_attempt['lesson_title']
                 lesson_url = f'https://dvmn.org{new_attempt["lesson_url"]}'
 
-                print(is_negative, lesson_title, lesson_url)
+                if is_negative:
+                    bot.send_message(chat_id=chat_id,
+                                     text=f'Работа "{lesson_title}" проверена.\n'
+                                     f'К сожалению, в работе были найдены ошибки.\n{lesson_url}')
+                else:
+                    bot.send_message(chat_id=chat_id,
+                                     text=f'Работа "{lesson_title}" проверена.\nПреподавателю всё понравилось!'
+                                     f' Можно приступать к следующему заданию!\n{lesson_url}')
 
             elif response['status'] == 'timeout':
                 params['timestamp'] = response['timestamp_to_request']
