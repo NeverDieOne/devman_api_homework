c2601d147d5e28a340e6991eb1c6b320595d624c
neverdieone <altiore@mail.ru>
2019-05-27 20:24:14 +0300

    Replace all code to main func.

--- main.py
+++ main.py
@@ -6,7 +6,26 @@ import logging
 
 
 def main():
+    load_dotenv()
+
+    bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
     chat_id = os.environ['CHAT_ID']
+
+    class MyLogsHandler(logging.Handler):
+
+        def __init__(self, bot, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.bot = bot
+
+        def emit(self, record):
+            log_entry = self.format(record)
+            self.bot.send_message(chat_id=os.environ['CHAT_ID'],
+                                  text=log_entry)
+
+    logger = logging.getLogger('Devman Logger')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(MyLogsHandler(bot))
+    logger.warning('Бот запущен!')
 
     while True:
         url = 'https://dvmn.org/api/long_polling/'
@@ -44,27 +63,5 @@ def main():
             logger.warning(f"Бот упал с ошибкой: {error}")
             continue
 
-
-class MyLogsHandler(logging.Handler):
-
-    def __init__(self, bot, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.bot = bot
-
-    def emit(self, record):
-        log_entry = self.format(record)
-        self.bot.send_message(chat_id=os.environ['CHAT_ID'],
-                              text=log_entry)
-
-
 if __name__ == '__main__':
-    load_dotenv()
-
-    bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
-
-    logger = logging.getLogger('Devman Logger')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(MyLogsHandler(bot))
-    logger.warning('Бот запущен!')
-
     main()
